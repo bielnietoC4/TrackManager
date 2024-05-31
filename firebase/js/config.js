85d5ee6d63eb5d6d42b2358323d7f52c09a6f540
BNJ <bielnietoC4@gmail.com>
2024-05-31 16:45:57 +0200

    arreglado

--- firebase/js/config.js
+++ firebase/js/config.js
@@ -1,10 +1,10 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAx0qq8mJ9EupbqYQuZ2FgDCjkg_-53F7U",
-  authDomain: "items-cdc96.firebaseapp.com",
-  projectId: "items-cdc96",
-  storageBucket: "items-cdc96.appspot.com",
-  messagingSenderId: "719177681259",
-  appId: "1:719177681259:web:9edd609788460b32a6a42e"
-};
+firebase.initializeApp({
+    apiKey: "AIzaSyAx0qq8mJ9EupbqYQuZ2FgDCjkg_-53F7U",
+    authDomain: "items-cdc96.firebaseapp.com",
+    projectId: "items-cdc96",
+    storageBucket: "items-cdc96.appspot.com",
+    messagingSenderId: "719177681259",
+    appId: "1:719177681259:web:71d18f1ebc74e9faa6a42e"
+});
 
 const auth = firebase.auth();
